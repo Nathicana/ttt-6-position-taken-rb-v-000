53e036f8f70a1c95805e0bae55c6800f47bc01a1
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-13 03:03:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,8 @@ def position_taken?(board, index)
 
  if board[index] == "  "|| nil|| ""
      return false
- if board[index] == "X"||"O"
+ else board[index] == "X"||"O"
         return true
 end
 end
-end
+
